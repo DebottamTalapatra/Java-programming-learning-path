0254eb33777b180ec4f9a9558b3e1092754752f4
Debottam Talapatra <talapatradebottam2004@gmail.com>
2025-08-22 19:00:05 +0530

    Day Three

---
+++ ArrayOfObjects/Main.java
@@ -0,0 +1,20 @@
+package ArrayOfObjects;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Car car1 = new Car("Mustang", "Red");
+        Car car2 = new Car("Corvette", "Blue");
+        Car car3 = new Car("Charger", "Yellow");
+
+        Car[] cars = { car1, car2, car3 };
+
+        // OR
+        // Car[] cars = { new Car("Mustang", "Red"), new Car("Corvette", "Blue"), new
+        // Car("Charger", "Yellow") };
+
+        for (Car car : cars) {
+            car.drive();
+        }
+    }
+}
